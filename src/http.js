1433849c19169f1b3fa35a64329715c0202c3efa
Bryan C. Winter <bryancwinter@gmail.com>
2020-03-08 14:20:33 -0400

    Initial Commit

--- src/http.js
+++ src/http.js
@@ -1,4 +1,49 @@
-import {add} from './helpers';
+import { add } from './helpers';
+import { Observable } from 'rxjs';
+
+add.li("BEGIN");
+
+/* Promise Pattern */
+const p = new Promise(
+	( resolve, reject ) => {
+		setTimeout(
+			() => {
+				resolve("Promise");
+			} , 1000 
+		)
+	}
+);
+
+p.then(
+	( message ) => {
+		add.li( message )
+	} 
+)
+
+// Observable subscribe( next, error, complete )
+const o = new Observable(
+	( observer ) => {
+		setInterval(
+			() => { 
+				observer.next('Observable');
+			}
+		, 1000 )
+	}
+);
+
+const $subscribed = o.subscribe(
+	{
+		next : ( message ) => add.li( message ),
+		error: ( error ) => ( console.log( error ) ),
+		complete : () => add.li("This observable is complete ")
+	}
+)
+
+setTimeout(
+	() => {
+		$subscribed.unsubscribe();
+	} , 2000 
+)
 
 
-
+add.li("END");
